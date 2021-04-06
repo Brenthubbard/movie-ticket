a34ef8af8dde840f91d0e198824f76f391f68e05
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-06 17:33:13 -0600

    add more functions

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,19 @@ function Ticket(movieTime, movieAge, viewerAge) { //builds object with values #3
 Ticket.prototype.ticketPrice = function () { //calc ticket price using method on object return price #5
 
   let myPrice = this.movieTime + this.movieAge + this.viewerAge;
+  if ($("#popcorn").is(":checked")) {
+    myPrice += 12;
+    }
+
+  if ($("#candy").is(":checked")) {
+    myPrice += 8;
+  }
+
+  if ($("#drink").is(":checked")) {
+    myPrice += 6;
+  }
   return myPrice;
+
 }
 
 
@@ -23,6 +35,9 @@ $(document).ready(function () {
     const convertMovieTime = parseInt($("#movie-time").val()); //gathering values from userForm #1
     const convertMovieAge = parseInt($("#movie-age").val());
     const convertViewerAge = parseInt($("#viewer-age").val());
+    const convertPopcorn = parseInt($("#popcorn").val());
+    const convertCandy = parseInt($("#candy").val());
+    const convertDrink = parseInt($("#drink").val());
 
     let moviePrice = new Ticket(convertMovieTime, convertMovieAge, convertViewerAge) //calling constructor and passing values gathered #2
     $("#output").text(`Your movie will start at ${$("#movie-time option:selected").text()} and cost $${moviePrice.ticketPrice()}.00`); //call ticket price after object construction #4
